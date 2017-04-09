65298a8fa01ff5840eb8bdc2c3fd1ea90057a841
Timothy Ubbens <timothy.ubbens@gmail.com>
2017-04-08 21:11:19 -0400

    Add API version 2 - do something useful

--- app.py
+++ app.py
@@ -2,11 +2,36 @@
 """A simple API to do almost nothing"""
 import hug
 
-@hug.local()
+users = [
+    {
+        'uid': u'jsmith',
+        'name': u'John Smith',
+        'email': u'jsmith@example.com',
+        'phone': u'123-456-7890'
+    },
+    {
+        'uid': u'jdoe',
+        'name': u'Jane Doe',
+        'email': u'jdoe@example.com',
+        'phone': u'234-567-8901'
+    },
+    {
+        'uid': u'ssample',
+        'name': u'Sally Sample',
+        'email': u'ssample@example.com',
+        'phone': u'345-678-9012'
+    }
+]
+
 @hug.get('/users', versions=1)
 def user(user_id):
     return 'I do nothing useful.'
     #return user_id
+
+@hug.local()
+@hug.get('/users', versions=2)
+def user():
+    return {'users': users}
 
 if __name__ == '__main__':
     user.interface.local()
